a00bb009b33f74420430a74497cb41a821e8aff6
draxya <68575901+draxya@users.noreply.github.com>
2022-09-07 18:26:18 +0300

    tag update

--- commands/stat.js
+++ commands/stat.js
@@ -9,17 +9,18 @@ module.exports.run = async (client, message) => {
   const duration = moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
 
   const embed = new MessageEmbed()
-    .setColor('YELLOW')
-    .setTitle('Storyteller İstatistikler')
+    .setColor('DARK_PURPLE')
+    .setTitle(client.user.username +   ' İstatistikler')
     .addFields(
       { name: ':timer: Gecikme', value: `${client.ws.ping.toFixed(2)}  ms`, inline: true },
       { name: ':construction_worker: Çalışma Süresi', value: `${duration}`, inline: true },
       { name: ':busts_in_silhouette: Kullanıcılar', value: `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}`, inline: true },
       { name: ':tv: Kanallar', value: `${client.channels.cache.size}`, inline: true },
+      { name: ':clipboard: Sunucular', value: `${client.guilds.cache.size}`, inline: true },
       { name: ':desktop: Bellek Kullanımı (MB)', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}`, inline: true },
     )
     .setTimestamp()
-    .setFooter({ text: 'Aesir', iconURL: client.user.avatarURL() });
+    .setFooter({ text: client.user.username, iconURL: client.user.avatarURL() });
   message.reply({ embeds: [embed] });
 };
 exports.conf = {
